fe04e13cd7345314037fcf4f74216cf107faef0b
Songphon <encodetype@gmail.com>
2021-02-01 14:28:04 +0700

    stream morgan to winston logger

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const logger = require("./modules/winston-logger");
+const stripAnsi = require("strip-ansi");
 
 // routers
 const globalRouterHandler = require("./modules/global-router-handler");
@@ -11,6 +12,15 @@ const bodyParserJson = require("./modules/body-parser-handler");
 const allRouters = require("./routers");
 
 const rateLimiter = require("./modules/rate-limiter");
+
+const stream = require("stream");
+
+// TODO: move to right directory
+const morganStream = new stream.Writable();
+morganStream._write = (chunk, encoding, callback) => {
+  logger.info(stripAnsi(chunk.toString("utf-8")));
+  callback(null);
+};
 
 const start = async (PORT) => {
   const app = express();
@@ -21,7 +31,11 @@ const start = async (PORT) => {
   app.use(helmet());
   app.use(bodyParserJson());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(morgan("dev"));
+  app.use(
+    morgan("dev", {
+      stream: morganStream,
+    })
+  );
 
   app.all("/", globalRouterHandler.caseLived);
   app.use(allRouters);
